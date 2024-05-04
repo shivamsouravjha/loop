a8991ebba3e9e188892b400dd0979caeeec1293c
shivamsouravjha <shivamsouravjha@gmail.com>
2024-05-04 10:58:15 +0000

    remove:print

--- app/database/dataSync.py
+++ app/database/dataSync.py
@@ -30,6 +30,5 @@ def load_csv_to_db(csv_file_path,table_name,index_file_path ):
         if not chunk.empty:
             chunk.to_sql(table_name, con=sync_engine, if_exists='append', index=False)
         # Update the last processed line
-        print(chunk)
         start_line += len(chunk)
     write_last_processed_line(index_file_path, start_line)
